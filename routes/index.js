1b9ee4ab45e017a4c183e792637f44c299529c9f
Sunil Unka <sunil.unka@gmail.com>
2015-10-06 18:35:11 -0400

    Completed basic rendering.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,26 @@
 var express = require('express')
 var router = express.Router();
 var bodyparser = require('body-parser');
+var Hotel = require('../models').Hotel;
+var Activity = require('../models').Activity;
+var Restaurant = require('../models').Restaurant;
+var Place = require('../models').Place;
+
+
 
 
 router.get('/', function(req, res, next){
-  res.render("index", function(err, data){
-    res.status(200).send(data);
-  })
+  Hotel.find({}).exec().then(function(hotels) {
+      Restaurant.find({}).exec().then(function(restaurants) {
+          Activity.find({}).exec().then(function(activities) {
+              res.render('index', {
+                  all_hotels: hotels,
+                  all_restaurants: restaurants,
+                  all_activities: activities
+              });
+          }).then(null, console.log);
+      }).then(null, console.log);
+  }).then(null, console.log);
 })
 
 
