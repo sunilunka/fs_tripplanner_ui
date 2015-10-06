1b7a3b295a39ed0e3a64196a6c2a7ecb90116b8d
Sunil Unka <sunil.unka@gmail.com>
2015-10-06 18:20:39 -0400

    Completed setting up models and seeding database.

--- models/index.js
+++ models/index.js
@@ -1,15 +1,51 @@
 var mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 mongoose.connect('mongodb://localhost/tripplanner');
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, "mongodb connection error:"));
 
 
 var placeSchema = new mongoose.Schema({
-  address: { type: String },
-  city: { type: String },
-  state: { type: String },
-  phone: { type: String },
-  location: { type: }
+  address: [String],
+  city: [String],
+  state: [ String ],
+  phone: [ String ],
+  location: [ Number, Number]
 
 
 });
+
+var min = [1, "The number of stars has to be between one and five"];
+var max = [5, "The number of stars has to be between one and five"]
+
+var hotelSchema = new mongoose.Schema({
+  name: String, 
+  place: [placeSchema],
+  num_stars: { type: Number, min: min, max: max },
+  amenities: String
+})
+
+var activitySchema = new mongoose.Schema({
+  name: String,
+  place: [placeSchema],
+  age_range: { type: String }
+})
+
+var restaurantSchema = new mongoose.Schema({
+  name: String, 
+  place: [placeSchema],
+  cuisines: String,
+  price: { type: Number, min: min, max: max }
+})
+
+Place = mongoose.model("Place", placeSchema);
+Hotel = mongoose.model("Hotel", hotelSchema);
+Activity = mongoose.model("Activity", activitySchema);
+Restaurant = mongoose.model("Restaurant", restaurantSchema)
+
+module.exports = {
+  Place: Place,
+  Hotel: Hotel, 
+  Activity: Activity,
+  Restaurant: Restaurant
+}
